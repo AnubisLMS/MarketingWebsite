56b466bf903d6c304f61dd48ff468c4d2de2222e
Mei Shin Lee <53961253+meishinlee@users.noreply.github.com>
2022-02-25 16:01:15 -0500

    UPDATE search bar to be dynamic

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import {useState, useEffect} from 'react';
 
 // Let's assume that we have a list of blog titles that are passed in 
 // Create a filter and rank the ones that are most relevant by count of word 
 // in the title.
+
 const blogTitles = ["Anubis Development Guide", "Anubis Contributor Guide", "Anubis User Guide", 
 "Student User Manual", "Faculty User Manual", "Navigation Information", "Celebrating the 20th Anniversary of Anubis", 
 "Bug Fixes and Updates", "New Features and Updates", "Announcements", "2022 Look Ahead"]; 
@@ -12,10 +14,11 @@ const blogTitles = ["Anubis Development Guide", "Anubis Contributor Guide", "Anu
 // It performs a case insensitive search on the title of the blog and returns
 // a list of blogs that match the filter term. If no blogs are found, it returns
 // "No blogs found".
-function filterBlogs(filterTerm, blogTitles){
+function filterBlogs(searchTerm, blogTitles){
+  
     var filteredBlogTitles = []; 
     for (var i = 0; i < blogTitles.length; i++){
-        if (blogTitles[i].toLowerCase().includes(filterTerm.toLowerCase())){
+        if (blogTitles[i].toLowerCase().includes(searchTerm.toLowerCase())){
             filteredBlogTitles.push(blogTitles[i]);
         }
     }
@@ -25,19 +28,21 @@ function filterBlogs(filterTerm, blogTitles){
     return filteredBlogTitles;
 }
 
+
 const Blog = () => {
+  
+const searchItems = (searchValue) => {
+  setSearchInput(searchValue);
+}
+  const [searchInput, setSearchInput] = useState('');
   var filteredBlogsList = filterBlogs("Anubis", blogTitles);
-  let filteredBlogs=filteredBlogsList.map((item,index)=>{
+  console.log("ssss",searchInput);
+  let filteredBlogs = filteredBlogsList.map((item,index)=>{
     return <li key={index}>{item}</li>
   })
-  const BlogPost = () => {
-    return (
-      <div>
-      </div>
-    )
-  }
+
   return (
-    console.log(filteredBlogs),
+    console.log("1231231",filteredBlogs),
     <Layout>
       <div className='sm:w-fit lg:w-max'>
 
@@ -47,13 +52,16 @@ const Blog = () => {
           <h1 className='text-4xl font-semibold'>Anubis<span className='text-primary'> Blog</span> </h1>
           <p className='text-xl text-gray'>A collection of posts by our developers and maintainers.</p>
           <div class="mb-3 xl:w-96">
-            <div class="input-group relative flex flex-wrap items-stretch w-full mb-4">
-              <input type="search" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-black bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-black focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon3"/>
-              <button class="btn inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" type="button" id="button-addon3">Search</button>
+          <div class="input-group relative flex flex-wrap items-stretch w-full mb-4">
+              <input type="search" onChange = {(e) => { console.log(e.target); 
+                searchItems(e.target.value) }} class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-black bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-black focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon3"/>
+              {/* <button onClick={(e) => { console.log(e.target); 
+                searchItems(e.target.value) }} class="btn inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" type="submit" id="button-addon3">Search</button> */}
             </div>
           </div>
           <ul>
-            {filteredBlogs}
+            {/* need a for loop, for blog in blogList, check if searchTerm.included in blog.title */}
+            {/* {filteredBlogs_list} */}
           </ul>
         </div>
         <div className='flex flex-col items-center'>

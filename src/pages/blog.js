62cc16af118e76e6f1b7fb1321bba296efefd38e
ampahwa <a.pahwa.m@gmail.com>
2022-02-28 18:57:34 -0500

    ADD minor syling

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -22,35 +22,22 @@ const Blog = ({ data }) => {
           <h1 className='text-4xl font-semibold'>Anubis<span className='text-primary'> Blog</span> </h1>
           <p className='text-xl text-gray'>A collection of posts by our developers and maintainers.</p>
         </div>
-        <div className='flex flex-col items-center'>
-          <div/>
-        </div>
-        <div>
-          
-          {posts.map(post => {
-            const {title, date, description} = post.frontmatter
-
-            return (
-              
-              <header>
-                <li>
-                <h2>
-                  <Link to={`/blog/${post.frontmatter.slug}`} itemProp="url">
-                    <span itemProp="headline">{title}</span>
-                  </Link>
-                </h2>
-                    <span itemProp="date">{date}</span>
-                    <p>
-                    <span itemProp="description">{description}</span>
-                    </p>
-                  
-                </li>
-              </header>
-              
-            )
-          })}
-        
-        </div>
+          <div className='flex flex-col items-center'>
+            {posts.map(post => {
+              const {title, date, description} = post.frontmatter
+                return (
+                  <div className='w-max mt-16'>
+                  <h2 className='text-4xl font-bold text-primary'>
+                    <Link to={`blog/${post.frontmatter.slug}`} itemProp="url">
+                      <span itemProp="headline">{title}</span>
+                    </Link>
+                  </h2>
+                      <span itemProp="date" className='text-gray'>{date}</span>
+                      <p itemProp="description" className='text-xl text-gray'>{description}</p> 
+                  </div>
+                )
+            })}     
+          </div>
       </div>
     </Layout>
     )

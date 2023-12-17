30a48018e67e514df11aa5bf878a58a165edd7ea
Sudhanshu Pandey <sp6370@nyu.edu>
2023-12-17 19:30:29 +0000

    update: added old css config to new project

--- marketing-website/tailwind.config.js
+++ marketing-website/tailwind.config.js
@@ -6,13 +6,50 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    padding: {
+      sm: '8px',
+      md: '16px',
+      lg: '24px',
+      xl: '48px',
+      '2xl': '95px',
+      '3xl': '190px',
     },
+    screens: {
+      xs: '482px',
+      sm: '640px',
+      md: '768px',
+      mdlg: '900px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      maxWidth: {
+        max: '1200px',
+      },
+      width: {
+        'max': '1200px',
+      },
+      fontSize: {
+        'base-smaller': '12px',
+        base: '14px',
+        'base-larger': '16px',
+      },
+      colors: {
+        primary: '#5685F5',
+        black: '#0D1117',
+        gray: '#89929b',
+        'gray-2': '#c6cdd5',
+        'gray-3': '#161B22',
+        white: '#ECF2F8',
+        red: '#FA7970',
+        orange: '#FAA356',
+        green: '#7CE38B',
+        cyan: '#A2D2FB',
+        blue: '#77BDFB',
+        purple: '#CEA5FB',
+      }
+    }
   },
   plugins: [],
 }
